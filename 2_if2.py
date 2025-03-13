fc77d7a12dc27f5159070db8eb5a60e6d3d8cef5
Vladimir Kulakov <vladimirkulakov1980@gmail.com>
2025-03-13 13:21:30 +0300

    Второе задание первой домашки

--- 2_if2.py
+++ 2_if2.py
@@ -20,7 +20,23 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    def two_strings(str1,str2):
+        str1_type = type(str1)
+        str2_type = type(str2)
+        str1_len = len(str(str1))
+        str2_len = len(str(str2))
+        if str1_type is not str or str2_type is not str:
+            return 0
+        if str1 == str2:
+            return 1
+        if str1 != str2 and str1_len > str2_len:
+            return 2
+        if str1 != str2 and str2 == 'learn':
+            return 3
+    print(two_strings('moscow python',3))
+    print(two_strings('moscow python','moscow python'))
+    print(two_strings('moscow python','python'))
+    print(two_strings('study','learn'))
 
 if __name__ == "__main__":
     main()
